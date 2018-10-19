d9effc09636d4f5f22bc9d85042bee6b8e0c4a75
Josef Vidlák <vidlec@gmail.com>
2018-10-19 22:29:57 +0200

    gh-pages

--- src/utils/verifyImage.js
+++ src/utils/verifyImage.js
@@ -1,9 +1,13 @@
 import getBase64 from "./getBase64";
+import axios from "axios";
 
 export default (image, name) => {
   return new Promise((resolve, reject) => {
     getBase64(image, base64 => {
-      resolve(true);
+      return axios.post(
+        "https://jfuzi5ahih.execute-api.eu-west-1.amazonaws.com/dev/recognize",
+        base64
+      );
     });
   });
 };
